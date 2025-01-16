a73af3b4b8decbc7270786adcf2ba84069e360e6
gkotecki <gkotecki@users.noreply.github.com>
2025-01-15 21:52:43 -0300

    ADD: day 16 part 2

--- day/16/part2.ts
+++ day/16/part2.ts
@@ -10,76 +10,102 @@ const end = matrix
   .flatMap((row, r) => row.map((cell, c) => ({ cell, r, c })))
   .find(({ cell }) => cell === 'E')!
 
-const startStr = `${start.r},${start.c}`
-const endStr = `${end.r},${end.c}`
-
-printMatrix(matrix)
-
-console.log({ start, end })
-
-type Direction = 'up' | 'down' | 'left' | 'right'
-const directionCost = {
-  up: { up: 0, down: 2000, left: 1000, right: 1000 },
-  down: { up: 2000, down: 0, left: 1000, right: 1000 },
-  left: { up: 1000, down: 1000, left: 0, right: 2000 },
-  right: { up: 1000, down: 1000, left: 2000, right: 0 },
-}
-
-function getNeighbors([r, c]: [number, number], direction: Direction) {
-  const neighbors: [number, number, Direction, number][] = []
-  if (matrix[r + 1][c] !== '#') neighbors.push([r + 1, c, 'down', directionCost[direction].down])
-  if (matrix[r - 1][c] !== '#') neighbors.push([r - 1, c, 'up', directionCost[direction].up])
-  if (matrix[r][c + 1] !== '#') neighbors.push([r, c + 1, 'right', directionCost[direction].right])
-  if (matrix[r][c - 1] !== '#') neighbors.push([r, c - 1, 'left', directionCost[direction].left])
-  return neighbors
-}
-
-const frontier: [number, number, Direction][] = []
-frontier.push([start.r, start.c, 'right'])
-
-const cameFrom = new Map<string, string | undefined>()
-cameFrom.set(startStr, undefined)
-
-const costSoFar = new Map<string, number>()
-costSoFar.set(startStr, 0)
-
-while (frontier.length > 0) {
-  const [currR, currC, currDir] = frontier.pop()!
-  const currentStr = `${currR},${currC}`
-
-  for (const [nextR, nextC, nextDir, turnCost] of getNeighbors([currR, currC], currDir)) {
-    const nextStr = `${nextR},${nextC}`
-    let newCost = costSoFar.get(currentStr)! + 1 + turnCost
-
-    if (!costSoFar.has(nextStr) || newCost < costSoFar.get(nextStr)!) {
-      frontier.push([nextR, nextC, nextDir])
-      costSoFar.set(nextStr, newCost)
-      cameFrom.set(nextStr, currentStr)
-    }
+class Node {
+  constructor(
+    public row: number,
+    public col: number,
+    public dirR: number,
+    public dirC: number,
+    public previous: Node | null,
+  ) {}
+  get key() {
+    return `${this.row},${this.col},${this.dirR},${this.dirC}`
+  }
+  get coords() {
+    return [this.row, this.col, this.dirR, this.dirC]
+  }
+  get pathCoords(): string[] {
+    const [r, c] = this.coords
+    const coordKey = `${r},${c}`
+    if (!this.previous) return [coordKey]
+    return [...this.previous.pathCoords, coordKey]
   }
 }
 
-const path: string[] = []
-let current = endStr
-while (true) {
-  path.push(current)
-  current = cameFrom.get(current)!
-  if (current === startStr) {
-    break
+class PriorityQueue {
+  private elements: [Node, number][] = []
+  add(node: Node, priority: number) {
+    this.elements.push([node, priority])
+    this.elements.sort(([, a], [, b]) => a - b)
+  }
+  grab() {
+    return this.elements.shift()
+  }
+  get size() {
+    return this.elements.length
   }
 }
 
-matrix.forEach((row, r) => {
-  row.forEach((cell, c) => {
-    if (['E', 'S'].includes(matrix[r][c])) return
-    // if (path.includes(`${r},${c}`)) matrix[r][c] = ','
-    if (![',', 'E', 'S'].includes(matrix[r][c]) && costSoFar.has(`${r},${c}`))
-      matrix[r][c] = costSoFar.get(`${r},${c}`)!.toString()
-  })
-})
-
 printMatrix(matrix)
 
-console.log('cost so far:')
-console.log(costSoFar.get(endStr), '===', 7036, ' pt1 ex1 ok:', costSoFar.get(endStr) === 7036)
-console.log(costSoFar.get(endStr), '===', 11048, ' pt1 ex2 ok:', costSoFar.get(endStr) === 11048)
+const startNode = new Node(start.r, start.c, 0, 1, null)
+const endNode = new Node(end.r, end.c, 0, 0, null)
+
+const frontier = new PriorityQueue()
+frontier.add(startNode, 0)
+
+const seen = new Set<string>()
+
+const foundCosts: number[] = []
+const finalNodes = new Set<string>()
+
+let step = 0
+while (frontier.size > 0) {
+  step++
+  if (step % 1000 == 0) {
+    await new Promise((res) => setTimeout(res, 5))
+    console.clear()
+    printMatrix(matrix)
+    console.log('foundCosts', foundCosts)
+  }
+
+  const [current, cost] = frontier.grab()!
+  seen.add(current.key)
+
+  const [r, c, dr, dc] = current.coords
+
+  if (matrix[r][c] === '.') {
+    matrix[r][c] = ','
+  }
+
+  if (cost > foundCosts[0]) {
+    continue
+  }
+
+  if (matrix[r][c] === 'E') {
+    foundCosts.push(cost)
+    foundCosts.sort()
+    current.pathCoords.forEach((coord) => finalNodes.add(coord))
+    continue
+  }
+
+  for (const [newR, newC, newDirR, newDirC, newCost] of [
+    [r + dr, c + dc, dr, dc, cost + 1],
+    [r, c, dc, -dr, cost + 1000],
+    [r, c, -dc, dr, cost + 1000],
+  ]) {
+    const next = new Node(newR, newC, newDirR, newDirC, current)
+    if (matrix[next.row][next.col] === '#') continue
+    if (seen.has(next.key)) continue
+    frontier.add(next, newCost)
+  }
+}
+
+for (const key of finalNodes) {
+  const [r, c] = key.split(',').map(Number)
+  if (matrix[r][c] == ',') matrix[r][c] = 'X'
+}
+printMatrix(matrix)
+
+console.log({ foundCosts })
+console.log({ finalNodes: finalNodes.size })
